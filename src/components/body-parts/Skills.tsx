bbb89282c7c4a7f4be54fb3f40666109dd7d2731
Rupert De Ruyver <rupert.deruyver@ugent.be>
2025-08-21 23:02:14 +0200

    Update skill types in data.json; refactor Skills component to display skills by type

--- src/components/body-parts/Skills.tsx
+++ src/components/body-parts/Skills.tsx
@@ -1,6 +1,12 @@
 import { Code } from "lucide-react";
 import data from "../../../public/data.json";
 
+const colors: Record<string, string> = {
+  technical: "bg-blue-100 text-blue-800",
+  "programming-language": "bg-green-100 text-green-800",
+  soft: "bg-red-100 text-red-800",
+};
+
 function Skills() {
   return (
     <>
@@ -8,30 +14,30 @@ function Skills() {
         <Code className="w-5 h-5 text-blue-600" />
         <h3 className="text-2xl font-bold text-gray-800">Skills</h3>
       </div>
-      <div className="grid md:grid-cols-1 gap-6">
-        <div>
-          <h4 className="font-semibold text-gray-700 mb-3">Technical Skills</h4>
-          <div className="flex flex-wrap gap-2">
-            {Object.values(data.skills).map((skill) => (
-              <span className="px-3 py-1 bg-blue-100 text-blue-800 rounded-full text-sm font-medium">
-                {skill.name}
-              </span>
-            ))}
-          </div>
-        </div>
-        {/* <div>
-            <h4 className="font-semibold text-gray-700 mb-3">Soft Skills</h4>
+      <div
+        className="grid gap-6 grid-cols-3"
+        // className={`grid gap-6 grid-cols-${
+        //   Object.values(data.skill_types).length
+        // }`}
+      >
+        {Object.entries(data.skill_types).map(([type_id, skill_type]) => (
+          <div>
+            <h4 className="font-semibold text-gray-700 mb-3">
+              {skill_type.name}
+            </h4>
             <div className="flex flex-wrap gap-2">
-              {skills.soft.map((skill, index) => (
-                <span
-                  key={index}
-                  className="px-3 py-1 bg-green-100 text-green-800 rounded-full text-sm font-medium"
-                >
-                  {skill}
-                </span>
-              ))}
+              {Object.values(data.skills)
+                .filter((skill) => skill.type === type_id)
+                .map((skill) => (
+                  <span
+                    className={`px-3 py-1 ${colors[type_id]} rounded-full text-sm font-medium`}
+                  >
+                    {skill.name}
+                  </span>
+                ))}
             </div>
-          </div> */}
+          </div>
+        ))}
       </div>
     </>
   );
