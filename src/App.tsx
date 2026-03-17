17bff293319fbf838faa1d915e40253827d83dbd
Rupert De Ruyver <rupert.deruyver@ugent.be>
2026-03-17 20:27:34 +0100

    refactor: Remove CV page and associated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
 import Detail from "./pages/Detail";
-import CV from "./pages/CV";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeScreen />} />
       <Route path="/:type/:id" element={<Detail />} />
-      <Route path="/cv" element={<CV />} />
     </Routes>
   );
 }
